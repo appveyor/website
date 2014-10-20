b1612e521efbf5fe7cb26d33b1fbe539bd4c5085
Feodor Fitsner <feodor@fitsner.com>
2014-10-19 19:22:21 -0700

    Initial checkin

---
+++ NJekyll/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace NJekyll.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("<h1>Hello, world!</h1>" + Styles.Render("~/Content/css"), "text/html");
+        }
+    }
+}
