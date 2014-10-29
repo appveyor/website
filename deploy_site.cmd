fee45bb4ccd3e45832766a91ca0f4c9548251949
Feodor Fitsner <feodor@fitsner.com>
2014-10-28 18:24:12 -0700

    Fixed script

--- deploy_site.cmd
+++ deploy_site.cmd
@@ -1,3 +1,3 @@
 if exist %DEPLOYMENT_TARGET%\site rd %DEPLOYMENT_TARGET%\site /S /Q
-%DEPLOYMENT_TARGET%\site
+md %DEPLOYMENT_TARGET%\site
 xcopy %DEPLOYMENT_SOURCE%\njekyll\site\*.* %DEPLOYMENT_TARGET%\site /Y /S
