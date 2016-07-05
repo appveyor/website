9dd1c1e6f25d0ee3bfad79d4999d7b65d05b6107
XhmikosR <xhmikosr@gmail.com>
2016-07-05 14:35:13 +0300

    Load JS asynchronously.
    Also, switch to the universal Google Analytics code.

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    "use strict";
+    $(document).foundation();
+    addAnchors('.docs-content h2, .docs-content h3, .docs-content h4, .docs-content h5, .docs-content h6');
+})(jQuery);
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-32029905-1', 'auto');
+ga('send', 'pageview');
