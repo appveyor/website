dc5507b0c4685677a5eca40df876fb4419668cf0
XhmikosR <xhmikosr@gmail.com>
2016-08-01 08:50:57 +0300

    Fix mobile topbar padding.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,6 +5,14 @@
     "use strict";
 
     (function() {
+        var docEl = document.documentElement;
+        var classRE = new RegExp("(^|\\s)no-js(\\s|$)");
+        var className = docEl.className;
+
+        docEl.className = className.replace(classRE, "$1js$2");
+    })();
+
+    (function() {
 
         var testimonialsContainer = document.getElementById("testimonials");
 
