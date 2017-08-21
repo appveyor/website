a8d0f15d0660d295a4f8958a38836993eef6bc26
Feodor Fitsner <feodor@appveyor.com>
2017-08-20 20:23:58 -0700

    Disable accessibility test

--- Gruntfile.js
+++ Gruntfile.js
@@ -365,7 +365,7 @@ module.exports = function(grunt) {
         'eslint',
         'markdownlint',
         'htmllint',
-        'accessibility',
+        //'accessibility',
         'connect:linkChecker',
         'linkChecker'
     ]);
